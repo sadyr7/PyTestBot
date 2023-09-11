88f6241018c6744685174a3493f8b3361dba146c
sadyr7 <sadyr.top@gmail.com>
2023-09-11 23:02:46 +0600

    2

--- task/management/commands/bot.py
+++ task/management/commands/bot.py
@@ -1,16 +1,10 @@
 from django.core.management.base import BaseCommand
-from task import models
 from task.models import Task_Backend, Task_Frontend
-from config import settings
 from telebot import TeleBot, types
-import requests
-
 
 bot = TeleBot('6435207550:AAEZglHbN6cVmhuYi1hyoOBsHGf1mvN2Re8', threaded=False)
 
-
 class Command(BaseCommand):
-    help = 'Implemented to Django application telegram wwe.py setup command'
 
     def handle(self, *args, **kwargs):
         bot.enable_save_next_step_handlers(delay=2)
@@ -33,16 +27,14 @@ def start_message(message):
     )
 
 
-
 @bot.message_handler(func=lambda x: x.text in ['BackEnd'])
 def task_backend(message):
     keyboard = types.ReplyKeyboardMarkup()
     button3 = types.KeyboardButton('Таски Backend')
-    button4 = types.KeyboardButton('Хватит')
+    button4 = types.KeyboardButton('Назад')
     keyboard.add(button3, button4)
 
     message3 = bot.send_message(message.chat.id, 'Начинаем BackEnd задачи', reply_markup=keyboard)
-
 
 current_task_Backend_id = 1
 
@@ -60,16 +52,14 @@ def task_backend_next(message, *args, **kwargs):
     with open(current_task.image.path, 'rb') as image_file:
         bot.send_photo(message.chat.id, image_file)
 
-
 @bot.message_handler(func=lambda x: x.text in ['Frontend'])
 def task_frontend(message):
     keyboard = types.ReplyKeyboardMarkup()
     button5 = types.KeyboardButton('Таски Frontend')
-    button6 = types.KeyboardButton('Хватит')
+    button6 = types.KeyboardButton('Назад')
     keyboard.add(button5, button6)
 
     message3 = bot.send_message(message.chat.id, 'Начинаем Frontend задачи', reply_markup=keyboard)
-
 
 current_tasks_Frontend_id = 1
 
@@ -86,6 +76,20 @@ def task_frontend_next(message, *args, **kwargs):
 
     with open(current_tasks.image.path, 'rb') as image_file:
         bot.send_photo(message.chat.id, image_file)
+
+
+@bot.message_handler(func=lambda x: x.text == 'Назад')
+def go_back(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    button1 = types.KeyboardButton('BackEnd')
+    button2 = types.KeyboardButton('Frontend')
+    keyboard.add(button1, button2)
+
+    bot.send_message(
+        message.chat.id,
+        'Вы вернулись в главное меню.',
+        reply_markup=keyboard
+    )
 
 
 bot.polling()
